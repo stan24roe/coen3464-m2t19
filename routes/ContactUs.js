fd2ffe2bfdb0aa38d5f74d174575f7a97ba3fa6c
Stan Roe Rosales <stan24roe@gmail.com>
2017-02-27 14:53:52 +0800

    initial commit

--- routes/ContactUs.js
+++ routes/ContactUs.js
@@ -9,16 +9,17 @@ router.get('/', function(req, res, next) {
 
 router.post('/send', function(req, res, next){
     var transporter = nodemailer.createTransport({
-        service: 'Gmail',
+      service: 'Gmail',
         auth: {
-            user: 'coen3464t19@gmail.com',
+           // user: 'coenteam19@gyahoo.com',
+           user: 'coen3464t19@gmail.com',
             pass: 'pass1234567890',
         }
     });
 
     var mailOptions = {
-        from: 'JillyBeans <coen3463@gmail.com>',
-        to: 'coen3463@gmail.com',
+        from: 'coen3464t19@gmail.com',
+        to: 'coen3464t19@gmail.com',
         subject: 'inquiries',
         text: 'you have a new message... Name: '+req.body.name+ ' Email: '+req.body.email+ ' Message: '+req.body.message,
         html: '<p>You got a new message with the following details..</p><ul><li>Name: '+req.body.name+ '</li><li>Email: '+req.body.email+ '</li><li>Message: '+req.body.message+ '</li></ul>'
